3fd637578389cd980e6944ef25690fb5f7ab3c08
azezmohammed <muhamadaziz047@gmail.com>
2020-09-08 14:44:33 +0700

    Javascript login fixed

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -1,29 +1,62 @@
-let loginBtn = document.getElementById("login-btn");
-loginBtn.addEventListener("click", login);
+// --------------- ELEMENTS ----------------
+let button = document.getElementById("button");
 
-function login() {
-  let usernameInput = document.getElementById("username").value;
-  let passwordInput = document.getElementById("password").value;
-  console.log("tes", usernameInput, passwordInput);
+// --------------- FETCH ----------------
+let endpoint = `https://5f52d4f27c47c30016e30a68.mockapi.io/tuur/Users`;
 
-  // buat fetch dulu
-  fetch("https://5f52d4f27c47c30016e30a68.mockapi.io/tuur/Users")
-    .then((response) => response.json())
-    .then((result) => {
-      let user = result.filter((item) => item.username === usernameInput);
-      console.log("hasil user setelah filter", user);
-      if (user.length > 0) {
-        alert("username anda ditemukan");
-        if (user[0].password === passwordInput) {
-          // simpan data user yang login ke localstorage
-          localStorage.setItem("user", JSON.stringify(user[0]));
-          alert("username dan password sama");
-          window.location.replace("/index.html");
-        } else {
-          alert("password anda salah");
-        }
+// --------------- Functions ----------------
+async function login() {
+  try {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let response = await fetch(endpoint);
+    let results = await response.json();
+
+    // check email dari user data dan value email
+    let user = results.filter((result) => result.email === email);
+    if (user.length > 0) {
+      //email terdaftar
+      // check password dari user data dan value password
+      if (user[0].password === password) {
+        // simpan data ke localStorage
+        localStorage.setItem("user", JSON.stringify(user[0]));
+        // redirect ke home
+        window.location.href = "/index.html";
       } else {
-        alert("username tidak ditemukan");
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Wrong email or password!",
+          showConfirmButton: true,
+          // timer: 5000,
+        });
+        // alert("Wrong email or password!");
       }
-    });
+    } else if (email == "" && password == "") {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Please fill email and password!",
+        showConfirmButton: true,
+        // timer: 5000,
+      });
+    } else {
+      // ketika email tidak ketemu by filter method
+      Swal.fire({
+        position: "center",
+        icon: "warning",
+        title: "Your email has not been registered yet",
+        showConfirmButton: true,
+        // timer: 5000,
+      });
+      // alert("Your email has not been registered yet");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
+// --------------- EVENT LISTENER ----------------
+button.addEventListener("click", function () {
+  login();
+});
