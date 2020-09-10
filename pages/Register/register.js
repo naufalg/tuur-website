1b1a6d0d2ea6c4e0b6c5c7b8c7eaf8863f79befa
azezmohammed <muhamadaziz047@gmail.com>
2020-09-10 16:53:52 +0700

    Add confirm password not fix it

--- pages/Register/register.js
+++ pages/Register/register.js
@@ -8,6 +8,7 @@ let endpoint = `https://5f52d4f27c47c30016e30a68.mockapi.io/tuur/Users`;
 // --------------- Functions ----------------
 async function register() {
   try {
+    // ! Memanggil Dom Variabel
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -30,6 +31,7 @@ async function register() {
       console.log(users);
       let registeredUser = users.filter((user) => user.email === email);
 
+      // ! Ketika User sudah terdaftar
       if (registeredUser.length > 0) {
         Swal.fire({
           position: "center",
@@ -37,6 +39,8 @@ async function register() {
           title: "Email anda sudah terdaftar coba menggunakan email yang lain",
           showConfirmButton: true,
         });
+
+        // ! User baru mendaftar
       } else {
         let response = await fetch(endpoint, options);
         console.log(response.json());
@@ -50,7 +54,14 @@ async function register() {
         ok.addEventListener("click", function () {
           window.location.href = "../Login/login.html";
         });
+
+        // ! user confirm password
       }
+      // else (password.value != confirm_password.value) {
+      //   confirm_password.setCustomValidity("Passwords Don't Match");
+      // }
+
+      // ! Jika user tidak melengkapi semua
     } else {
       Swal.fire({
         position: "center",
@@ -63,6 +74,25 @@ async function register() {
     console.error(error);
   }
 }
+
+// // ! Confirm password
+// // Dom
+// let password = document.getElementById("password");
+// let confirm_password = document.getElementById("confirm_password");
+
+// // Function
+// function validatePassword() {
+//   //   Jika password tidak sama
+//   if (password.value != confirm_password.value) {
+//     confirm_password.setCustomValidity("Passwords Don't Match");
+//     //     Jika password sama
+//   } else {
+//     confirm_password.setCustomValidity("");
+//   }
+// }
+
+// password.onchange = validatePassword;
+// confirm_password.onkeyup = validatePassword;
 // --------------- event listener ----------------
 signupBtn.addEventListener("click", function () {
   register();
